b122de2409f7a1c1059c112afda8c48aeb312bcd
BuiltByFrancis <builtbyfrancis@outlook.com>
2023-02-14 23:24:51 +0000

    Deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,90 @@
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+const TokenAddress = {
+    1: "",
+    5: "0x34D7be09C299f2364212c96335467f4dA01b35C7",
+};
+
+const RewardAddress = {
+    1: "",
+    5: "0x3446D487135A59125A27180842a5Bb1899947247",
+};
+
+const AuctionAddress = {
+    1: "",
+    5: "0xDB145c4B17bD109c04488B52709f9D972a73464D",
+};
+
+async function main() {
+    const { chainId } = await ethers.provider.getNetwork();
+    if(chainId == 5) {
+        await DeployTestToken();
+    }
+
+    await DeployAuction(chainId);
+
+    console.log("Done");
+}
+
+async function DeployAuction(chainId) {
+    var address = AuctionAddress[chainId];
+    if (address != "") {
+        console.log(`Auction already deployed at ${address}`);
+        return;
+    }
+
+    const tokenAddress = TokenAddress[chainId];
+    const rewardAddress = RewardAddress[chainId];
+    const endTimeStamp = 1676500493;
+
+    const Auction = await ethers.getContractFactory("Auction");
+    const auction = await Auction.deploy(tokenAddress, rewardAddress, endTimeStamp);
+
+    await auction.deployed();
+
+    AuctionAddress[chainId] = auction.address;
+    console.log(`Auction deployed to ${auction.address}`);
+
+    console.log("Sleeping before verify...");
+    await sleep(120000);
+
+    await hre.run("verify:verify", {
+        address: auction.address,
+        constructorArguments: [tokenAddress, rewardAddress, endTimeStamp],
+    });
+}
+
+async function DeployTestToken() {
+    const chainId = 5;
+    var address = TokenAddress[chainId];
+    if (address != "") {
+        console.log(`Token already deployed at ${address}`);
+        return;
+    }
+
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+
+    await token.deployed();
+
+    TokenAddress[chainId] = token.address;
+    console.log(`Token deployed to ${token.address}`);
+
+    console.log("Sleeping before verify...");
+    await sleep(120000);
+
+    await hre.run("verify:verify", {
+        address: token.address,
+        constructorArguments: [],
+    });
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
